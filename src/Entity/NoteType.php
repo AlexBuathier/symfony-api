5106c1123a96dacdfe75f2da733a95a15e94bedd
alex <alexandre.buathier@free.fr>
2022-06-27 13:58:32 +0200

    API Plateform NoteType: Add NoteExpense groups & limit Collection & Item operations

--- src/Entity/NoteType.php
+++ src/Entity/NoteType.php
@@ -7,17 +7,20 @@ use App\Repository\NoteTypeRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
 
 #[ORM\Entity(repositoryClass: NoteTypeRepository::class)]
-#[ApiResource]
+#[ApiResource (itemOperations: ["GET"], collectionOperations: [])]
 class NoteType
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column(type: 'integer')]
+    #[Groups("note-expense:read")]
     private $id;
 
     #[ORM\Column(type: 'string', length: 10)]
+    #[Groups("note-expense:read")]
     private $name;
 
     #[ORM\OneToMany(mappedBy: 'noteType', targetEntity: NoteExpense::class)]
